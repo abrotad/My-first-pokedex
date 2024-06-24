0b164b9fd5d058be455e99aa1a416a7cbd3a8ec3
Abrham Mengistu <abrotad@gmail.com>
2024-06-24 12:29:17 -0700

    added pokemon images

--- index.js
+++ index.js
@@ -15,10 +15,51 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const response = await fetch(url);
     const pokemon = await response.json();
-    console.log(pokemon);
+    // console.log(pokemon);
+    createPokemonCard(pokemon);
   } catch (error) {
     console.log(error);
   }
 };
 
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+  // console.log(pokemonEl)
+
+  const pokemonInnerHTML = `
+<div class="img-container">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+            alt=""
+          />
+        </div>
+        <div class="info">
+          <span class="number">${pokemon.id}</span>
+          <h3 class="name">${pokemon.name}</h3>
+          <small class="type"> Type <span>${pokemon.types[0].type.name}</span></small>
+        </div>
+`;
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  poke_container.appendChild(pokemonEl);
+  console.log(pokemonEl);
+};
 fetchPokemons();
+
+/*
+*   <div id="poke-container" class="poke-container">
+      <div class="pokemon">
+        <div class="img-container">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+            alt=""
+          />
+        </div>
+        <div class="info">
+          <span class="number">#001</span>
+          <h3 class="name">Balbasaur</h3>
+          <small class="type"> Type <span>grass</span></small>
+        </div>
+      </div>
+    </div>
+*/
